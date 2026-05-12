f57d0e73f91e49ca97e6a8db2ac607b956f0fb33
LEISHIQIANG <lei3184854661@gmail.com>
2026-05-12 20:43:21 +0800

    Expose message box suppression state

--- include/ModernMsgBox.h
+++ include/ModernMsgBox.h
@@ -5,4 +5,8 @@
 class ModernMsgBox {
 public:
     static int Show(HWND parent, const std::wstring& text, const std::wstring& title, UINT type);
+    static void SetSuppressed(bool suppressed);
+    static bool IsSuppressed();
+private:
+    static bool s_suppressed;
 };
